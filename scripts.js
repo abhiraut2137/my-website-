be86cd06e548363b1668d3f6fc646a17e6ec77aa
abhiraut2137 <122555063+abhiraut2137@users.noreply.github.com>
2024-10-07 00:09:06 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+ // Handle the form submission
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your message. I will get back to you soon!');
+});
